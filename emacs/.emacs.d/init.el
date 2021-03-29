18839b38d31e2871f4ce762d2087bba2bdf7dff8
JaHIY <jaklsy@gmail.com>
2021-03-29 23:31:32 +0800

    set font size in emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -24,17 +24,17 @@
   (cond
     ((string-equal system-type "windows-nt") ; Microsoft Windows
      (cond
-       ((member "Fira Code Retina" (font-family-list)) "Fira Code Retina")
-       ((member "Consolas" (font-family-list)) "Consolas")))
+       ((member "Fira Code Retina" (font-family-list)) "Fira Code Retina-12")
+       ((member "Consolas" (font-family-list)) "Consolas-12")))
     ((string-equal system-type "darwin") ; macOS
      (cond
-       ((member "Fira Code Retina" (font-family-list)) "Fira Code Retina")
-       ((member "Menlo" (font-family-list)) "Menlo")
-       ((member "Monaco" (font-family-list)) "Monaco")))
+       ((member "Fira Code Retina" (font-family-list)) "Fira Code Retina-12")
+       ((member "Menlo" (font-family-list)) "Menlo-12")
+       ((member "Monaco" (font-family-list)) "Monaco-12")))
     ((string-equal system-type "gnu/linux") ; linux
      (cond
-       ((member "Fira Code Retina" (font-family-list)) "Fira Code Retina")
-       ((member "DejaVu Sans Mono" (font-family-list)) "DejaVu Sans Mono"))))
+       ((member "Fira Code Retina" (font-family-list)) "Fira Code Retina-12")
+       ((member "DejaVu Sans Mono" (font-family-list)) "DejaVu Sans Mono-12"))))
   t
   t)
 
@@ -46,20 +46,20 @@
     (cond
       ((string-equal system-type "windows-nt")
        (cond
-         ((member "Sarasa Mono SC" (font-family-list)) "Sarasa Mono SC")
-         ((member "Microsoft YaHei" (font-family-list)) "Microsoft YaHei")
-         ((member "Microsoft JhengHei" (font-family-list)) "Microsoft JhengHei")
-         ((member "SimHei" (font-family-list)) "SimHei")))
+         ((member "Sarasa Mono SC" (font-family-list)) "Sarasa Mono SC-12")
+         ((member "Microsoft YaHei" (font-family-list)) "Microsoft YaHei-12")
+         ((member "Microsoft JhengHei" (font-family-list)) "Microsoft JhengHei-12")
+         ((member "SimHei" (font-family-list)) "SimHei-12")))
       ((string-equal system-type "darwin")
        (cond
-         ((member "Sarasa Mono SC" (font-family-list)) "Sarasa Mono SC")
-         ((member "Heiti SC" (font-family-list)) "Heiti SC")
-         ((member "Heiti TC" (font-family-list)) "Heiti TC")))
+         ((member "Sarasa Mono SC" (font-family-list)) "Sarasa Mono SC-12")
+         ((member "Heiti SC" (font-family-list)) "Heiti SC-12")
+         ((member "Heiti TC" (font-family-list)) "Heiti TC-12")))
       ((string-equal system-type "gnu/linux")
        (cond
-         ((member "Sarasa Mono SC" (font-family-list)) "Sarasa Mono SC")
-         ((member "Source Han Sans" (font-family-list)) "Source Han Sans")
-         ((member "WenQuanYi Micro Hei" (font-family-list)) "WenQuanYi Micro Hei"))))))
+         ((member "Sarasa Mono SC" (font-family-list)) "Sarasa Mono SC-12")
+         ((member "Source Han Sans" (font-family-list)) "Source Han Sans-12")
+         ((member "WenQuanYi Micro Hei" (font-family-list)) "WenQuanYi Micro Hei-12"))))))
 
 ;; set font for emoji
 (when (fboundp 'set-fontset-font)
@@ -67,11 +67,11 @@
     t
     '(#x1f300 . #x1fad0)
     (cond
-      ((member "Noto Color Emoji" (font-family-list)) "Noto Color Emoji")
-      ((member "Noto Emoji" (font-family-list)) "Noto Emoji")
-      ((member "Segoe UI Emoji" (font-family-list)) "Segoe UI Emoji")
-      ((member "Symbola" (font-family-list)) "Symbola")
-      ((member "Apple Color Emoji" (font-family-list)) "Apple Color Emoji"))
+      ((member "Noto Color Emoji" (font-family-list)) "Noto Color Emoji-12")
+      ((member "Noto Emoji" (font-family-list)) "Noto Emoji-12")
+      ((member "Segoe UI Emoji" (font-family-list)) "Segoe UI Emoji-12")
+      ((member "Symbola" (font-family-list)) "Symbola-12")
+      ((member "Apple Color Emoji" (font-family-list)) "Apple Color Emoji-12"))
     ;; Apple Color Emoji should be before Symbola, but Richard Stallman disabled it.
     ;; GNU Emacs Removes Color Emoji Support on the Mac
     ;; http://ergoemacs.org/misc/emacs_macos_emoji.html
