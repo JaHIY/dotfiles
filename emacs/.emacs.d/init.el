44f567979ed9ac06349ee73e430837c4bdf7fd5b
JaHIY <jaklsy@gmail.com>
2022-03-06 21:31:43 +0800

    fix bug in emacs init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -61,8 +61,7 @@
              :unless (display-graphic-p)
              :config
              (menu-bar-mode -1)
-             (tool-bar-mode -1)
-             (scroll-bar-mode -1))
+             (tool-bar-mode -1))
 
 (use-package emacs
              :if (display-graphic-p)
